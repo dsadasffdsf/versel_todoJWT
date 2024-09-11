1f22258756eca922f737e7acfb7a1bfdf7f17117
dsadasffdsf <qwertfdsazxcvg122@mail.ru>
2024-09-11 12:52:10 +0300

    update

--- service/user-service.js
+++ service/user-service.js
@@ -1,5 +1,5 @@
 const UserModel = require('../models/user-model');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const uuid = require('uuid');
 const mailService = require('./mail-service');
 const tokenService = require('./token-service');
